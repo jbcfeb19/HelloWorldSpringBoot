934d02cb1590bec76925243593da296e7d784bb1
romielisse <romielisse@gmail.com>
2019-02-19 14:51:01 -0500

    adding tables and links

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,12 +2,24 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/index")
+    @GetMapping("/")
     public String getIndex(){
         return "index";
+    }
+
+    @RequestMapping("/index2")
+    public String getIndex2(){
+        return "index2";
+    }
+
+
+    @GetMapping("/bslesson")
+    public String getBSLesson(){
+        return "bslesson";
     }
 }
