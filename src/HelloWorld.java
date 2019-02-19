3216432b126389ac91ab6c7f1d025990a12ff93b
romielisse <romielisse@gmail.com>
2019-02-19 09:49:40 -0500

    initial commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class HelloWorld {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello world!");
 
         // Declare an integer variable called num and
